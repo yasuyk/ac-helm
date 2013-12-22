c1bda4918a74a973b7b5941914cba038bd9535ae
Yasuyuki Oka <yasuyk@gmail.com>
2013-12-22 16:03:04 +0900

    Move changeling comments to CHANGLOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,22 @@
+# Changelog
+
+## 2.1 (04/12/2013)
+- Add a missing autoload cookie to `ac-complete-with-helm'.
+
+## 2.0 (04/11/2013)
+- Add persistent-action to show documentation.
+
+## 1.9 (04/02/2013)
+- Remove "Warning: `delete-backward-char'".
+
+    replace delete-backward-char to delete-char.
+
+## 1.8 (04/01/2013)
+- Fix bug that don't show current completion by overlay.
+
+## 1.7 (03/29/2013)
+
+- port to helm.
+
+    The original source code is below:
+    http://www.emacswiki.org/cgi-bin/wiki/download/ac-anything.el
--- ac-helm.el
+++ ac-helm.el
@@ -52,47 +52,6 @@
 
 ;; That's all.
 
-;;; History:
-
-;; Version 2.1  2013/04/12 Yasuyuki Oka <yasuyk@gmail.com>
-;; * Add a missing autoload cookie to `ac-complete-with-helm'
-;;
-;; Version 2.0  2013/04/11 Yasuyuki Oka <yasuyk@gmail.com>
-;; * Add persistent-action to show documentation.
-;;
-;; Version 1.9  2013/04/02 Yasuyuki Oka <yasuyk@gmail.com>
-;; * Remove "Warning: `delete-backward-char'"
-;;   replace delete-backward-char to delete-char
-;;
-;; Version 1.8  2013/04/01 Yasuyuki Oka <yasuyk@gmail.com>
-;; * Fix bug that don't show current completion by overlay
-;;
-;; Version 1.7  2013/03/29 Yasuyuki Oka <yasuyk@gmail.com>
-;; * port to helm.
-;;   The original source code is below:
-;;   http://www.emacswiki.org/cgi-bin/wiki/download/ac-anything.el
-;;
-;; $Log: ac-anything.el,v $
-;; Revision 1.6  2009/11/11 17:13:11  rubikitch
-;; Use pulldown.el if available
-;;
-;; Revision 1.5  2009/11/11 17:08:16  rubikitch
-;; Replace ac-prefix with (anything-attr 'ac-prefix)
-;;
-;; Revision 1.4  2009/04/18 21:08:49  rubikitch
-;; Remove attribute `ac-point'
-;;
-;; Revision 1.3  2009/04/18 21:03:51  rubikitch
-;; * Auto Document
-;; * Use anything-show-completion.el if available
-;;
-;; Revision 1.2  2009/02/09 21:24:44  rubikitch
-;; *** empty log message ***
-;;
-;; Revision 1.1  2009/02/09 21:09:16  rubikitch
-;; Initial revision
-;;
-
 ;;; Code:
 
 (require 'helm)
